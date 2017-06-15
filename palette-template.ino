08624e70d76715e3995e89c66fffe2ffca7ba939
Sean Cross <sean@xobs.io>
2017-06-15 15:04:52 +0800

    palette-template: remove debug #defines
    These are now stashed in the README file for future reference.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- palette-template.ino
+++ palette-template.ino
@@ -1,36 +1,6 @@
 #include "Arduino.h"
 #include "ChibiOS.h"
 
-#define PIN0_EFFECT FADE
-#define PIN0_SPEED 5
-#define PIN0_RANDOMNESS 0
-#define PIN0_BRIGHTNESS 100
-
-#define PIN1_EFFECT FADE
-#define PIN1_SPEED 4
-#define PIN1_RANDOMNESS 0
-#define PIN1_BRIGHTNESS 100
-
-#define PIN2_EFFECT FADE
-#define PIN2_SPEED 3
-#define PIN2_RANDOMNESS 0
-#define PIN2_BRIGHTNESS 100
-
-#define PIN3_EFFECT HEARTBEAT
-#define PIN3_SPEED 8
-#define PIN3_RANDOMNESS 0
-#define PIN3_BRIGHTNESS 100
-
-#define PIN4_EFFECT HEARTBEAT
-#define PIN4_SPEED 5
-#define PIN4_RANDOMNESS 0
-#define PIN4_BRIGHTNESS 100
-
-#define PIN5_EFFECT TWINKLE
-#define PIN5_SPEED 5
-#define PIN5_RANDOMNESS 0
-#define PIN5_BRIGHTNESS 100
-
 // Love to Code
 // Effects Template
 enum effects {
